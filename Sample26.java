29cf53aa50d20cdf884c5941545b5a15ca971a99
Makoto Tanabe <xekid78@outlook.com>
2018-01-23 18:05:19 +0900

    コレクション

--- Sample26.java
+++ Sample26.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 public class Sample26 {
 
 	public static void main(String[] args) {
+		// コレクション
 		ArrayList<Enemy2> enemies = new ArrayList<Enemy2>();
 		enemies.add(new Enemy2("モンスター"));
 		enemies.add(new Enemy2("ガイコツ"));
